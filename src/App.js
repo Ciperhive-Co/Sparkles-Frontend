46f508f06e3d6048241f75bbd261d17927a7cc4f
AhmedNazeer <iamahmad9311@gmail.com>
2023-08-11 17:43:54 +0500

    DEPLOY

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import clean from './clean-car.png'
-import dirty from './dirty-car.png'
+import clean from './cleancar.png';
+import dirty from './dirty-car.png';
 import Navbar from './Components/Navbar';
 import Slider from './Components/Slider';
 import Shop from './Components/Shop';
 import BlogSection from './Components/BlogSection';
 import ImageReel from './Components/ImageReel';
-import ReactCompareImage from "react-compare-image";
-import ClickablePictures from "./Components/ClickablePictures";
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+import ReactCompareImage from 'react-compare-image';
+import ClickablePictures from './Components/ClickablePictures';
 
 function App() {
 
-
   return (
-
-    
     <div className="App">
-        <div id='promotion' className="promotional-bar">
-          <div className="promotional-content">
-            <p>Get 10% off on all products. Use code: PROMO10</p>
-          </div>
-    </div>
+      <div id="promotion" className="promotional-bar">
+        <div className="promotional-content">
+          <p>Get 10% off on all products. Use code: PROMO10</p>
+        </div>
+      </div>
       <Navbar />
-      <Slider/>
-      <Shop/>
-      <ClickablePictures/>
-      <div className='mainWrapper'>
-      <div className='Wrapper'>
-      <ReactCompareImage leftImage= {clean} rightImage={dirty} sliderPositionPercentage = '0.021' sliderLineWidth = '1' sliderLineColor = '#24245a' leftImageCss = {{paddingBottom: '16.3%'}}/>
+      <Slider />
+      <Shop />
+      <ClickablePictures />
+      <div className="mainWrapper">
+        <div className="Wrapper" style={{ minHeight: '200px' }}>
+            <ReactCompareImage
+              leftImage={clean}
+              rightImage={dirty}
+              sliderPositionPercentage="0.021"
+              sliderLineWidth="3"
+              sliderLineColor="#24245a"
+              style={{
+                paddingBottom: '1%',
+                maxHeight: '20vh',
+              }}
+            />
+        </div>
       </div>
-      </div>
-      <BlogSection/>
-      <div className='instaHeading'>@ETHOSCARCARE</div>
-      <ImageReel/>
+      <BlogSection />
+      <div className="instaHeading">@ETHOSCARCARE</div>
+      <ImageReel />
     </div>
   );
 }
