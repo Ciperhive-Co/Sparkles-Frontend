a2e502441d0187a21a3927ed3f2d434b6034fca9
AhmedNazeer <iamahmad9311@gmail.com>
2023-08-16 23:22:09 +0500

    Main

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-<<<<<<< HEAD
-=======
-import clean from './cleancar.png';
-import dirty from './dirty-car.png';
->>>>>>> fe2cb2852a30351bb0dbff09b17e6946275d9bf3
 import Navbar from './Components/Navbar';
 import Slider from './Components/Slider';
 import Shop from './Components/Shop';
 import BlogSection from './Components/BlogSection';
 import ImageReel from './Components/ImageReel';
 import Footer from './Components/Footer';
-<<<<<<< HEAD
+
 import ClickablePictures from './Components/ClickablePictures';
 import {useState,useEffect} from "react";
 import ComparisonSlider from './Components/ComparisonSlider';
-=======
-import ReactCompareImage from 'react-compare-image';
-import ClickablePictures from './Components/ClickablePictures';
->>>>>>> fe2cb2852a30351bb0dbff09b17e6946275d9bf3
+
+
 
 // export default App;
 function App() {
   
   const [isLoading, setIsLoading] = useState(true);
 
-<<<<<<< HEAD
   useEffect(() => {
 
     //MAKE IT ASYNC AND LOAD YOUR DATA HERE//
@@ -45,8 +37,6 @@ function App() {
     );
   }
   
-=======
->>>>>>> fe2cb2852a30351bb0dbff09b17e6946275d9bf3
   return (
     <div className="App">
       <div id="promotion" className="promotional-bar">
@@ -58,27 +48,12 @@ function App() {
       <Slider />
       <Shop />
       <ClickablePictures />
-<<<<<<< HEAD
+
         <div className="Wrapper" style={{ minHeight: '200px' }}>
         <ComparisonSlider/>
         </div>
-=======
-      <div className="mainWrapper">
-        <div className="Wrapper" style={{ minHeight: '200px' }}>
-            <ReactCompareImage
-              leftImage={clean}
-              rightImage={dirty}
-              sliderPositionPercentage="0.021"
-              sliderLineWidth="3"
-              sliderLineColor="#24245a"
-              style={{
-                paddingBottom: '1%',
-                maxHeight: '20vh',
-              }}
-            />
-        </div>
-      </div>
->>>>>>> fe2cb2852a30351bb0dbff09b17e6946275d9bf3
+
+
       <BlogSection />
       <div className="instaHeading">@Sparkles.co</div>
       <ImageReel />
